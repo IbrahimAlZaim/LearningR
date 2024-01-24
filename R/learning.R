167111027e7c84b00a5abc0a899996ce1a94d3d7
Ibrahim AlZaim <ima@biomed.au.dk>
2024-01-24 10:25:55 +0100

    Added code to R/learning.R

--- R/learning.R
+++ R/learning.R
@@ -1,5 +1,30 @@
-
-# Loading packages --------------------------------------------------------
-
 library(tidyverse)
+library(NHANES)
 
+nhanes_small <- select(
+  NHANES, Age, Gender, BMI, Diabetes,
+  PhysActive, BPSysAve, BPDiaAve, Education
+)
+
+nhanes_small <- rename_with(nhanes_small, snakecase::to_snake_case)
+
+nhanes_small <- rename(nhanes_small, sex = gender)
+
+# Did we have to select here before renaming?
+nhanes_small %>%
+  select(phys_active) %>%
+  rename(physically_active = phys_active)
+
+nhanes_small %>%
+  select(bp_sys_ave, education)
+
+nhanes_small %>%
+  select(bp_sys_ave, bp_dia_ave) %>%
+  rename(bp_sys = bp_sys_ave, bp_dia = bp_dia_ave)
+
+nhanes_small %>%
+  select(bmi, contains("age"))
+
+nhanes_small %>%
+  select(starts_with("bp")) %>%
+  rename(bp_systolic = bp_sys_ave)
